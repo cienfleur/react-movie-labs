6fcd334184f88988eec7afda0582881f4748298b
cienfleur <redvelvet0214@protonmail.ch>
2024-10-04 16:30:05 +0100

    Added filtering of movie list - Movie lab 1 complete

--- movies/src/components/headerMovieList/index.js
+++ movies/src/components/headerMovieList/index.js
@@ -5,30 +5,26 @@ import Paper from "@mui/material/Paper";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 
-const Header = (props ) => {
-  const title = props.title
+const Header = (props) => {
+  const title = props.title;
   return (
-    <Paper 
-      component="div" 
+    <Paper
+      component="div"
       sx={{
         display: "flex",
         justifyContent: "space-around",
         flexWrap: "wrap",
         marginBottom: 1.5,
       }}
-      >
-      <IconButton
-        aria-label="go back"
-      >
+    >
+      <IconButton aria-label="go back">
         <ArrowBackIcon color="primary" fontSize="large" />
       </IconButton>
 
       <Typography variant="h4" component="h3">
         {title}
       </Typography>
-      <IconButton
-        aria-label="go forward"
-      >
+      <IconButton aria-label="go forward">
         <ArrowForwardIcon color="primary" fontSize="large" />
       </IconButton>
     </Paper>
